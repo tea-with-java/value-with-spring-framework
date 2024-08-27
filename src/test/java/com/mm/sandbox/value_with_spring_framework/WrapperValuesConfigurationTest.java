691ff85858f4713a08c8a89783a973e242f18cbe
Marina <tea.with.java@gmail.com>
2024-08-27 11:37:37 +0100

    added: fail context creation if properties are missing

--- src/test/java/com/mm/sandbox/value_with_spring_framework/WrapperValuesConfigurationTest.java
+++ src/test/java/com/mm/sandbox/value_with_spring_framework/WrapperValuesConfigurationTest.java
@@ -3,9 +3,11 @@ package com.mm.sandbox.value_with_spring_framework;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,6 +16,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class WrapperValuesConfigurationTest {
     @Autowired
     WrapperValuesConfiguration configuration;
+
+    @Value("${custom.integerWithDefaultValue}")
+    Integer integerWithDefaultValue;
 
     @Test
     void testInjectedValues() {
@@ -27,6 +32,10 @@ class WrapperValuesConfigurationTest {
 
         assertThat(configuration.get_Float()).isEqualTo(3.56f);
         assertThat(configuration.get_Double()).isEqualTo(-234.345);
+
+        assertThat(integerWithDefaultValue).isEqualTo(2024);
+
+
     }
 
     @Configuration
@@ -35,6 +44,13 @@ class WrapperValuesConfigurationTest {
         @Bean
         WrapperValuesConfiguration wrapperValuesConfiguration() {
             return new WrapperValuesConfiguration();
+        }
+
+        @Bean
+        static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+            var configurer = new PropertySourcesPlaceholderConfigurer();
+            configurer.setIgnoreUnresolvablePlaceholders(false);
+            return configurer;
         }
     }
 }
