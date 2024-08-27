53cd3269e471a0e8459f4898d19721fadcb0322f
Marina <tea.with.java@gmail.com>
2024-08-27 11:45:48 +0100

    added: configuration of collections via properties

---
+++ src/test/java/com/mm/sandbox/value_with_spring_framework/CollectionValuesConfigurationTest.java
@@ -0,0 +1,47 @@
+package com.mm.sandbox.value_with_spring_framework;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(
+        properties = {
+                "list=a,c,b"
+        }
+)
+public class CollectionValuesConfigurationTest {
+
+    @Value("${list}") Collection<String> collection;
+    @Value("${list}") List<String> list;
+    @Value("${list}") Set<String> set;
+
+    @Test
+    void testCollection() {
+        assertThat(collection).containsOnly("a,c,b");
+    }
+
+    @Test
+    void testList() {
+        assertThat(list).containsOnly("a,c,b");
+    }
+
+    @Test
+    void testSet() {
+        assertThat(set).containsOnly("a,c,b");
+    }
+
+    @Configuration
+    static class Config {}
+
+}
